81ed135c500f1c088158accf192ef50f98515bb9
Steve Turley <steve@turleylabs.com>
2020-11-11 13:08:34 -0600

    Update red_amber_green.rb

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,13 +1,9 @@
-
 lambda { |stdout,stderr,status|
-  output = stdout + stderr
-  if /SyntaxError:/.match(output)
-    :amber
-  elsif /^FAIL/.match(output)
-    :red
-  elsif /^PASS/.match(output)
-    :green
-  else
-    :amber
-  end
-}
+    output = stdout + stderr
+    pattern = /failed/
+    if match = pattern.match(output)
+      :red
+    else
+      :green
+    end
+  }
